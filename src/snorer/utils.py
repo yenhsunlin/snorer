82fe4a05e1e0e161c219dfe94301d3d02a3afd26
Yen-Hsun Lin <yenhsun@phys.ncku.edu.tw>
2024-04-26 15:27:49 +0800

    Create utils.py

---
+++ src/snorer/utils.py
@@ -0,0 +1,14 @@
+# Created by Yen-Hsun Lin (Academia Sinica) in 04/2024.
+# Copyright (c) 2024 Yen-Hsun Lin.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version (see <http://www.gnu.org/licenses/>).
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+from .constant import Constants
