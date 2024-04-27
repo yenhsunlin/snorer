019ad8202f5827395d561d62a91613a79a6d6520
Yen-Hsun Lin <yenhsun@phys.ncku.edu.tw>
2024-04-27 22:26:30 +0800

    Update geometry.py

--- src/snorer/geometry.py
+++ src/snorer/geometry.py
@@ -11,6 +11,10 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
+__all__ = ['Geometry',]
+
+
+#---------- Import required utilities ----------#
 
 from numpy import sqrt,isclose,cos,sin
 from .constant import Constants
